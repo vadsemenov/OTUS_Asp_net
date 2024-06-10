695cb72a9806e07ba12da81700081c10d91bbc66
Vadim Semenov <semenov@bimdance.io>
2024-06-10 17:08:38 +0300

    Add create, delete, update methods

--- PromoCodeFactory.Core/Abstractions/Repositories/IRepository.cs
+++ PromoCodeFactory.Core/Abstractions/Repositories/IRepository.cs
@@ -2,13 +2,20 @@
 using System.Collections.Generic;
 using System.Threading.Tasks;
 using PromoCodeFactory.Core.Domain;
+using PromoCodeFactory.Core.Domain.Administration;
 
 namespace PromoCodeFactory.Core.Abstractions.Repositories
 {
-    public interface IRepository<T> where T: BaseEntity
+    public interface IRepository<T> where T : BaseEntity
     {
+        Task<bool> CreateAsync(T entity);
+
         Task<IEnumerable<T>> GetAllAsync();
 
         Task<T> GetByIdAsync(Guid id);
+
+        Task<bool> UpdateAsync(T entity);
+
+        Task<bool> DeleteAsync(Guid id);
     }
 }
---
+++ PromoCodeFactory.WebHost/Map/RoleItemMapper.cs
@@ -0,0 +1,36 @@
+﻿using PromoCodeFactory.Core.Domain.Administration;
+using PromoCodeFactory.WebHost.Models;
+
+namespace PromoCodeFactory.WebHost.Map;
+
+public static class RoleItemMapper
+{
+    public static Role ToModel(this RoleItemRequest roleItemRequest)
+    {
+        return new Role
+        {
+            Id = roleItemRequest.Id,
+            Name = roleItemRequest.Name,
+            Description = roleItemRequest.Description,
+        };
+    }
+  
+    public static RoleItemResponse ToDtoResponse(this Role role)
+    {
+        return new RoleItemResponse
+        {
+            Id = role.Id,
+            Name = role.Name,
+            Description = role.Description
+        };
+    }
+    public static RoleItemRequest ToDtoRequest(this Role role)
+    {
+        return new RoleItemRequest
+        {
+            Id = role.Id,
+            Name = role.Name,
+            Description = role.Description
+        };
+    }
+}
