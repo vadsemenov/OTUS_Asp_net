8202ea215409001b1eb6068de08422cf531b93ca
Vadim Semenov <5587394@mail.ru>
2024-12-09 13:30:42 +0300

    Add Docker Task

--- 5.DockerDockerCompose/Otus.Teaching.PromoCodeFactory.WebHost/Startup.cs
+++ 5.DockerDockerCompose/Otus.Teaching.PromoCodeFactory.WebHost/Startup.cs
@@ -6,6 +6,7 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Otus.Teaching.PromoCodeFactory.Core.Abstractions.Repositories;
@@ -18,6 +19,13 @@ namespace Otus.Teaching.PromoCodeFactory.WebHost
 {
     public class Startup
     {
+        private readonly IConfiguration _configuration;
+
+        public Startup(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
@@ -28,7 +36,8 @@ namespace Otus.Teaching.PromoCodeFactory.WebHost
             services.AddScoped<IDbInitializer, EfDbInitializer>();
             services.AddDbContext<DataContext>(x =>
             {
-                x.UseSqlite("Filename=PromoCodeFactoryDb.sqlite");
+                // x.UseSqlite("Filename=PromoCodeFactoryDb.sqlite");
+                x.UseNpgsql(_configuration.GetConnectionString("PromoCodeFactoryDb"));
                 x.UseLazyLoadingProxies();
             });
 
